6d2ebeb1c690437a2174f955f8258848a64d8761
Richard Foster <richard@rpf.me>
2021-05-25 20:03:27 +0100

    Temporarily display random cards on the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,43 @@
 import Head from 'next/head'
+import dynamic from 'next/dynamic'
+import { useState } from 'react'
+import { FACES, SUITS, ICard } from '../types'
+
+function pick <T>(arr: readonly T[]): T {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
+
+const randomCard = (): ICard => {
+  return {
+    suit: pick(SUITS),
+    face: pick(FACES),
+  }
+}
+
+const randomCards = (num: number): ICard[] => {
+  const cards = []
+  for (let i = 0; i < num; i++) cards.push(randomCard())
+
+  return cards
+}
+
+const CardSequence = dynamic(() => import('../components/CardSequence'), { ssr: false })
 
 export default function Home() {
+  const [cards, setCards] = useState(randomCards(7))
+
   return (
     <div>
       <Head>
         <title>Poker Trainer</title>
       </Head>
+
+      <button onClick={() => setCards(randomCards(7))}>
+        change
+      </button>
+
+      <CardSequence cards={cards.slice(0,5)} />
+      <CardSequence cards={cards.slice(5,7)} />
     </div>
   )
 }
--- types/index.ts
+++ types/index.ts
@@ -1,23 +1,29 @@
-export type ICardSuit =
-  | 'clubs'
-  | 'diamonds'
-  | 'spades'
-  | 'hearts'
+export const SUITS = [
+  'clubs',
+  'diamonds',
+  'spades',
+  'hearts',
+] as const
 
-export type ICardFace =
-  | '2'
-  | '3'
-  | '4'
-  | '5'
-  | '6'
-  | '7'
-  | '8'
-  | '9'
-  | '10'
-  | 'J'
-  | 'Q'
-  | 'K'
-  | 'A'
+export const FACES = [
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'J',
+  'Q',
+  'K',
+  'A',
+] as const
+
+export type ICardSuit = typeof SUITS[number]
+
+export type ICardFace = typeof FACES[number]
 
 export type ICardDirection =
   | 'forwards'
