6afe5ea17f28b44cf61591a0f07a5769c07a8045
Richard Foster <richard@rpf.me>
2021-05-26 20:11:29 +0100

    Add favicon and icons

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>Poker Trainer</title>
+
+          <meta name="description" content="A tool for learning poker hands" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon.png" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default CustomDocument
