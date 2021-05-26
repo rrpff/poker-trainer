365cb81881933ee27b374ae11f4c15a9665314dd
Richard Foster <richard@rpf.me>
2021-05-26 17:53:44 +0100

    Ensure visible card images load first by delaying image preloads

--- components/PreloadCardImages.tsx
+++ components/PreloadCardImages.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 import { cardToDescription, createDeck } from '../lib/utils'
 
 const IMAGES = createDeck()
@@ -6,13 +7,29 @@ const IMAGES = createDeck()
   .map(description => `/cards/${description}.svg`)
 
 export const PreloadCardImages = () => {
+  const startPreload = useDelay(1500)
+
+  if (!startPreload) return null
+
   return (
     <Head>
       {IMAGES.map(src =>
-        <link rel="preload" as="image" href={src}></link>
+        <link key={src} rel="preload" as="image" href={src}></link>
       )}
     </Head>
   )
 }
 
 export default PreloadCardImages
+
+const useDelay = (ms: number) => {
+  const [ready, setReady] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setReady(() => true)
+    }, ms)
+  }, [])
+
+  return ready
+}
