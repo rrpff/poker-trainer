3a65bf9a2e85e5ffb87b8d7ffc718e36871a1e8b
Richard Foster <richard@rpf.me>
2021-05-26 03:35:25 +0100

    Add descriptions to formatted hand names

--- lib/formatPokerHandName.test.ts
+++ lib/formatPokerHandName.test.ts
@@ -2,16 +2,46 @@ import { formatPokerHandName } from './formatPokerHandName'
 import { IPokerHandName } from '../types'
 
 it.each([
-  { hand: 'royal_flush', expected: 'Royal Flush' },
-  { hand: 'straight_flush', expected: 'Straight Flush' },
-  { hand: 'four_of_a_kind', expected: 'Four of a Kind' },
-  { hand: 'full_house', expected: 'Full House' },
-  { hand: 'flush', expected: 'Flush' },
-  { hand: 'straight', expected: 'Straight' },
-  { hand: 'three_of_a_kind', expected: 'Three of a Kind' },
-  { hand: 'two_pair', expected: 'Two Pair' },
-  { hand: 'pair', expected: 'Pair' },
-  { hand: 'high_card', expected: 'High Card' },
+  {
+      hand: 'royal_flush',
+      expected: { name: 'Royal Flush', description: 'Ace, King, Queen, Jack, and Ten, all in the same suit' }
+  },
+  {
+      hand: 'straight_flush',
+      expected: { name: 'Straight Flush', description: 'Sequence of five cards, all in the same suit' }
+  },
+  {
+      hand: 'four_of_a_kind',
+      expected: { name: 'Four of a Kind', description: 'Four cards with the same face' }
+  },
+  {
+      hand: 'full_house',
+      expected: { name: 'Full House', description: 'Three of a Kind and a Pair' }
+  },
+  {
+      hand: 'flush',
+      expected: { name: 'Flush', description: 'Five cards of the same suit, but not in a sequence' }
+  },
+  {
+      hand: 'straight',
+      expected: { name: 'Straight', description: 'Five cards in a sequence, but not the same suit' }
+  },
+  {
+      hand: 'three_of_a_kind',
+      expected: { name: 'Three of a Kind', description: 'Three cards with the same face' }
+  },
+  {
+      hand: 'two_pair',
+      expected: { name: 'Two Pair', description: 'Not one Pair, but two' }
+  },
+  {
+      hand: 'pair',
+      expected: { name: 'Pair', description: 'Two cards with the same face' }
+  },
+  {
+      hand: 'high_card',
+      expected: { name: 'High Card', description: 'The highest face card, when there are no other matches. Ace high.' }
+  },
 ])('formats hand names correctly', ({ hand, expected }) => {
   expect(formatPokerHandName(hand as IPokerHandName)).toEqual(expected)
 })
--- lib/formatPokerHandName.ts
+++ lib/formatPokerHandName.ts
@@ -1,16 +1,26 @@
 import { IPokerHandName } from '../types'
 
-export const formatPokerHandName = (hand: IPokerHandName): string => {
+export const formatPokerHandName = (hand: IPokerHandName): { name: string, description: string } => {
   switch (hand) {
-    case 'royal_flush': return 'Royal Flush'
-    case 'straight_flush': return 'Straight Flush'
-    case 'four_of_a_kind': return 'Four of a Kind'
-    case 'full_house': return 'Full House'
-    case 'flush': return 'Flush'
-    case 'straight': return 'Straight'
-    case 'three_of_a_kind': return 'Three of a Kind'
-    case 'two_pair': return 'Two Pair'
-    case 'pair': return 'Pair'
-    case 'high_card': return 'High Card'
+    case 'royal_flush':
+      return { name: 'Royal Flush', description: 'Ace, King, Queen, Jack, and Ten, all in the same suit' }
+    case 'straight_flush':
+      return { name: 'Straight Flush', description: 'Sequence of five cards, all in the same suit' }
+    case 'four_of_a_kind':
+      return { name: 'Four of a Kind', description: 'Four cards with the same face' }
+    case 'full_house':
+      return { name: 'Full House', description: 'Three of a Kind and a Pair' }
+    case 'flush':
+      return { name: 'Flush', description: 'Five cards of the same suit, but not in a sequence' }
+    case 'straight':
+      return { name: 'Straight', description: 'Five cards in a sequence, but not the same suit' }
+    case 'three_of_a_kind':
+      return { name: 'Three of a Kind', description: 'Three cards with the same face' }
+    case 'two_pair':
+      return { name: 'Two Pair', description: 'Not one Pair, but two' }
+    case 'pair':
+      return { name: 'Pair', description: 'Two cards with the same face' }
+    case 'high_card':
+      return { name: 'High Card', description: 'The highest face card, when there are no other matches. Ace high.' }
   }
 }
