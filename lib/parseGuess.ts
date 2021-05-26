9da47199eac483fa33817a2f319776eb6909d608
Richard Foster <richard@rpf.me>
2021-05-26 03:08:25 +0100

    Support guesses like "high ace" or "high king"

--- lib/parseGuess.ts
+++ lib/parseGuess.ts
@@ -1,7 +1,9 @@
 import { closest, distance } from 'fastest-levenshtein'
 import { HAND_NAMES, IPokerHandName } from '../types'
 
-const STARTS_WITH_FACE_REGEX = /^(two|three|four|five|six|seven|eight|nine|ten|jack|queen|king|ace|[0-9])/g
+const FACE_MATCH = `(two|three|four|five|six|seven|eight|nine|ten|jack|queen|king|ace|[0-9])`
+const STARTS_WITH_FACE_REGEX = new RegExp(`^${FACE_MATCH}`, 'g')
+const ENDS_WITH_FACE_REGEX = new RegExp(`${FACE_MATCH}$`, 'g')
 
 export const parseGuess = (guess: string): IPokerHandName | null => {
   const cleaned = guess
@@ -17,7 +19,9 @@ export const parseGuess = (guess: string): IPokerHandName | null => {
   const bestMatchDistance = distance(bestMatch, cleaned)
   if (bestMatchDistance < 3) return bestMatch as IPokerHandName
 
-  const withoutFace = cleaned.replace(STARTS_WITH_FACE_REGEX, '')
+  const withoutFace = cleaned
+    .replace(STARTS_WITH_FACE_REGEX, '')
+    .replace(ENDS_WITH_FACE_REGEX, '')
   const withoutFaceDistance = distance('high_card', withoutFace + '_card')
   if (withoutFaceDistance < 3) return 'high_card'
 
