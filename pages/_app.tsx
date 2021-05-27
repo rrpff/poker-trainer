ad54a3fbbbbe40ecbb46a79c9e7b2a757c687513
Richard Foster <richard@rpf.me>
2021-05-27 22:13:14 +0100

    Add loading bar when changing pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from 'next/app'
+import Nprogress from 'nextjs-progressbar'
+import { Fragment } from 'react'
 import '../styles/globals.css'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <Fragment>
+      <Nprogress color="#1dd1a1" />
+      <Component {...pageProps} />
+    </Fragment>
   )
 }
 
